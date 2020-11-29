3400a058eb8765e990eff1f73d9a36e2cb9d680e
Ramy ElMallah <ramyelmallah@gmail.com>
2020-11-29 14:57:35 -0800

    RPi script autorestart at exceptions

--- RPi_Script/main.py
+++ RPi_Script/main.py
@@ -21,19 +21,21 @@ if __name__ == "__main__":
         portName = "COM2"
 
     serialPort = serial.Serial(portName, 115200 , timeout=1, bytesize=8, parity='N', stopbits=1)
-    if (not serialPort.isOpen()):
-        exit(1)
-    
-    try:
-        executer = PiExecuter(serialPort)
-        while(True):
-            executer.readSerial()
-    except Exception as err:
-        startBytes = bytes([STARTING_BYTE]*50)
-        serialPort.write(startBytes)
-        serialPort.write(("EXCEPTION: "+str(err)).encode("utf-8"))
-        serialPort.write(0x55)
-        serialPort.write(0x55)
-        print(str(err))
-        exit(1)
+    serialPort.set_buffer_size(rx_size = 10**8, tx_size = 10**3)
 
+    while(1):
+        if (not serialPort.isOpen()):
+            exit(1)
+    
+        try:
+            executer = PiExecuter(serialPort)
+            while(True):
+                executer.readSerial()
+        except Exception as err:
+            startBytes = bytes([STARTING_BYTE]*50)
+            serialPort.write(startBytes)
+            serialPort.write(("EXCEPTION: "+str(err)).encode("utf-8"))
+            serialPort.write(0x55)
+            serialPort.write(0x55)
+            print(str(err)) 
+            del(executer)           
