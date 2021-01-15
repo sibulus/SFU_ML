aa412ab319a154951c5a776afda44a31c3aa06b2
Ramy ElMallah <ramyelmallah@gmail.com>
2021-01-14 16:27:27 -0800

    GET IP minor bug fix

--- RPi_Script/utils/piexecuter.py
+++ RPi_Script/utils/piexecuter.py
@@ -106,7 +106,7 @@ class PiExecuter():
 
         elif command == "GET_IP":
             from subprocess import check_output
-            ackPayload = str(check_output(['hostname','-I']))
+            ackPayload = str(check_output(['hostname','-I'])).decode('utf-8').split(" ")[0]
 
         elif self.execState == ExecState.NotConnected:
             raise Exception("Wrong Exec State reached somehow: {}".format(self.execState))
